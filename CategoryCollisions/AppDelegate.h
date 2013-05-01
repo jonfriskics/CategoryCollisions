231d74d7806d51a64e676154483101bb60494556
Jon Friskics <jonfriskics@gmail.com>
2013-05-01 11:31:36 -0400

    Initial Commit

---
+++ CategoryCollisions/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CategoryCollisions
+//
+//  Created by Jon Friskics on 5/1/13.
+//  Copyright (c) 2013 Code School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
